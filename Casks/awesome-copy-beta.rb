16c74becb291995de09becb270b06c3effb97303
kindparkllc <kindparkllc@gmail.com>
2026-07-07 13:31:53 -0400

    Update awesome-copy-beta.rb

--- Casks/awesome-copy-beta.rb
+++ Casks/awesome-copy-beta.rb
@@ -2,7 +2,7 @@ cask "awesome-copy-beta" do
   version "7.0b6,381"
   sha256 "e1a4857fa508e424bdd20d35b028745f9b74fdc389b6ca2098e3e7ac54720b07"
 
-  url "https://github.com/kindparkllc/Awesome-Copy/releases/download/Beta/AwesomeCopy-\#{version.before_comma}-\#{version.after_comma}.dmg"
+  url "https://github.com/kindparkllc/Awesome-Copy/releases/download/Beta/AwesomeCopy-#{version.before_comma}-#{version.after_comma}.dmg"
   name "Awesome Copy"
   desc "Clipboard manager for Mac (Beta)"
   homepage "https://awesomecopy.app"
